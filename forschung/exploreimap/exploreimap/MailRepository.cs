95da9d641f7a07acbf0e432462cb3d264c5a0231
Ralf Westphal <info@ralfw.de>
2013-07-31 12:37:09 +0200

    Geforscht: Email per IMAP abholen

---
+++ forschung/exploreimap/exploreimap/MailRepository.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using ActiveUp.Net.Mail;
+using ActiveUp.Net.Security;
+
+namespace exploreimap
+{
+    public class MailRepository
+    {
+        public MailRepository(string mailServer, int port, bool useSSL, string username, string password)
+        {
+            if (useSSL)
+                Client.ConnectSsl(mailServer, port);
+            else
+                Client.Connect(mailServer, port);
+            Client.Login(username, password);
+        }
+ 
+
+        public IEnumerable<Message> GetAllMails(string mailBox)
+        {
+            return GetMails(mailBox, "ALL").Cast<Message>();
+        }
+ 
+        public IEnumerable<Message> GetUnreadMails(string mailBox)
+        {
+            return GetMails(mailBox, "UNSEEN").Cast<Message>();
+        }
+ 
+        private MessageCollection GetMails(string mailBox, string searchPhrase)
+        {
+            var mails = Client.SelectMailbox(mailBox);
+            var messages = mails.SearchParse(searchPhrase);
+            return messages;
+        }
+
+        
+        private Imap4Client _client = null;
+        private Imap4Client Client
+        {
+            get { return _client ?? (_client = new Imap4Client()); }
+        }
+    }
+}
---
+++ forschung/exploreimap/exploreimap/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ActiveUp.Net.Mail;
+
+namespace exploreimap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var mailserver = ConfigurationManager.AppSettings["mailserver"];
+            var mailserver_user = ConfigurationManager.AppSettings["mailserver_user"];
+            var mailserver_password = ConfigurationManager.AppSettings["mailserver_password"];
+
+            Console.WriteLine("server: {0}, user: {1}", mailserver, mailserver_user);
+
+            var rep = new MailRepository(mailserver, 143, false, mailserver_user, mailserver_password);
+            Console.WriteLine("connected");
+            foreach (var email in rep.GetAllMails("Inbox"))
+            {
+                Console.WriteLine("{0}->{1}:{2}\n{3}</p>", email.To[0].Email, email.From, email.Subject, email.BodyText.Text);
+            }
+        }
+    }
+}
