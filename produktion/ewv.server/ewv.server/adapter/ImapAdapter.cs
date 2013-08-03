adebaec19e66484e71a81819688e49176f800a8c
Ralf Westphal <info@ralfw.de>
2013-08-03 17:32:39 +0200

    Spiegelung mit Imap und Smtp

--- produktion/ewv.server/ewv.server/adapter/ImapAdapter.cs
+++ produktion/ewv.server/ewv.server/adapter/ImapAdapter.cs
@@ -1,38 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using ActiveUp.Net.Mail;
 using ewv.server.kontrakt;
 
 namespace ewv.server.adapter
 {
     internal class ImapAdapter : IDisposable
     {
+        private readonly Imap4Client _imap;
+
         public ImapAdapter(KonfigurationAdapter config)
         {
-            
+            _imap = new Imap4Client();
+            _imap.Connect(config["mailserver_receive"]);
+            _imap.Login(config["mailserver_user"], config["mailserver_password"]);
         }
+
 
         public IEnumerable<Email> Einplanungen_abholen()
         {
-            return new[]
-                {
-                    new Email
-                        {
-                            An = "in10tagen@emailwiedervorlage.de",
-                            Von = "ralfw08@gmail.com",
-                            Betreff = "Ein Betreff",
-                            Text = "Ein Email-Text"
-                        },
-                    new Email
-                        {
-                            An = "in5minuten@emailwiedervorlage.de",
-                            Von = "info@ralfw.de",
-                            Betreff = "Re: Ein Betreff",
-                            Text = "Ein Antwort-Text"
-                        }
-                };
+            var mailbox = _imap.SelectMailbox("Inbox");
+            var messages = mailbox.SearchParse("UNSEEN").Cast<Message>().ToArray();
+
+            Console.WriteLine("Einplanungen abgeholt: {0}", messages.Count());
+
+            return messages.Select(msg => new Email
+            {
+                An = msg.To[0].Email,
+                Von = msg.From.Email,
+                Betreff = msg.Subject,
+                Text = msg.BodyText.Text
+            }).ToArray();
         }
 
+
         public void Dispose()
-        {}
+        {
+            _imap.Close();
+            _imap.Disconnect();
+        }
     }
 }
